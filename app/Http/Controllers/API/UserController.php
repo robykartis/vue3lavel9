de169ffce4cdfe89016b87d9463c119dbf92ada0
robykartis <robykarti.s@gmail.com>
2022-06-14 14:54:38 +0700

    commit

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session as FacadesSession;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+        if (Auth::attempt($credentials)) {
+            $success = true;
+            $message = "User Login Successfully";
+        } else {
+            $success = false;
+            $message = "Unauthorized";
+        }
+        $response = [
+            'success' => $success,
+            'message' => $message
+        ];
+        return response()->json($response);
+    }
+    public function register(Request $request)
+    {
+        try {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            $success = false;
+            $message = "User Resgister Successfully";
+        } catch (\Illuminate\Database\QueryException $ex) {
+            $success = false;
+            $message = $ex->getMessage();
+        }
+        $response = [
+            'success' => $success,
+            'message' => $message
+        ];
+        return response()->json($response);
+    }
+    public function logout()
+    {
+        try {
+            FacadesSession::flush();
+            $success = true;
+            $message = "Logout Successfully";
+        } catch (\Illuminate\Database\QueryException $ex) {
+            $success = false;
+            $message = $ex->getMessage();
+        }
+        $response = [
+            'success' => $success,
+            'message' => $message
+        ];
+        return response()->json($response);
+    }
+}
---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    <img class="card-img-top" alt="" />
+    <div class="card-body">
+      <h4 class="card-title">{{ title }}</h4>
+      <p class="card-text">{{ version }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup: () => ({
+    title: "Helllo Vue From Laravel 9",
+    version: Version,
+  }),
+};
+</script>
+
+<style>
+</style>
